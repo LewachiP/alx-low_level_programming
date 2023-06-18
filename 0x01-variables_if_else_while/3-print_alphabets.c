d58691590df21beb625c0ddc627491481c56b3c6
LewachiP <management.legitgroup@gmail.com>
2023-06-18 12:40:30 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabets
+ *
+ * Return. Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	         int i;
+		 
+		 for (i = 0; i < 52; i++)
+		 {
+			 putchar(alp[i]);
+		 }
+		 return (0);
