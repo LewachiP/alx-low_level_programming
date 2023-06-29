139f284d5808434cb3b07813d0e3a02acfc447b8
LewachiP <management.legitgroup@gmail.com>
2023-06-29 05:52:43 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - reverse array of intergers
+ * @a: array
+ * @n: number of element of array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int t;
+
+	for (i = 0; i < n--; 1++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}
