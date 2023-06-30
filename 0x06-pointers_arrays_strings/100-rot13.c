7fbf699ce34347b13bffa641cb9bb0f8861a3f4e
LewachiP <management.legitgroup@gmail.com>
2023-06-30 05:18:15 -0700

    rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ * Return: *>
+ */
+
+char *rot13(char *>)
+{
+	int i;
+	int j;
+	char data[] =
+"ABCDEFGHIJKLMNOPQRSTUVWXZYabcdefghijklmnopqrstuvwxyz";
+"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+        for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < s2; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] == datarot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
